5d5ba6f8ae77ed019c610f3c1c9991ea53e5bc01
idriss bennouna <idrissbennouna76@gmail.com>
2025-12-24 23:08:57 +0100

    tp6

--- App.js
+++ App.js
@@ -3,18 +3,20 @@ import AuthProvider, { AuthContext } from "./context/AuthContext";
 import AppDrawer from "./navigation/AppDrawer";
 import LoginScreen from "./screens/LoginScreen";
 import { useContext } from "react";
-
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 function RootNavigator() {
-  const { user } = useContext(AuthContext);
-  return user ? <AppDrawer /> : <LoginScreen />;
+const { user } = useContext(AuthContext);
+return user ? <AppDrawer /> : <LoginScreen />;
 }
-
 export default function App() {
-  return (
-    <AuthProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </AuthProvider>
-  );
+return (
+ <Provider store={store}>
+ <AuthProvider>
+ <NavigationContainer>
+ <RootNavigator />
+ </NavigationContainer>
+ </AuthProvider>
+ </Provider>
+);
 }
